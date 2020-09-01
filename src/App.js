1cb9847d790c115db89fea6bc2713f2412c02795
Zohaldon <47535654+Zohaldon@users.noreply.github.com>
2020-08-31 21:17:58 -0400

    Added divs for product and cart with css style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
 import data from "./data.json";
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping cart</a>
-      </header>
-      <main>Product List</main>
-      <footer>All rights are reserved</footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      product: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">Products</div>
+            <div className="sidebar">Cart items</div>
+          </div>
+        </main>
+        <footer>All rights are reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
