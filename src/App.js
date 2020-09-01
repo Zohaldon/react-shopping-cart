78ffc3aef3dd298037efe9d95f0c00aac8f2e350
Zohaldon <47535654+Zohaldon@users.noreply.github.com>
2020-08-31 21:33:00 -0400

    Products component finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import data from "./data.json";
+import Products from "./components/Products";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      product: data.products,
+      products: data.products,
       size: "",
       sort: "",
     };
@@ -19,7 +20,9 @@ class App extends React.Component {
         </header>
         <main>
           <div className="content">
-            <div className="main">Products</div>
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
             <div className="sidebar">Cart items</div>
           </div>
         </main>
